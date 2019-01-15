28ff481ac6cf7f7d7a3715cd045ce15fc97995f5
Mikhail Tchernychev <mikhail_tchernyhev@yahoo.com>
2019-01-14 20:01:03 -0800

    initial commit

---
+++ src/test1.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : test1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	// test 1
+	return 0;
+}
