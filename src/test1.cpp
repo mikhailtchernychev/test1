24135ca16342ecfa23094db85bd49fd86b6f0d75
Mikhail Tchernychev <mikhail_tchernyhev@yahoo.com>
2019-01-15 20:17:58 -0800

    using std::cout

--- src/test1.cpp
+++ src/test1.cpp
@@ -10,8 +10,12 @@
 using namespace std;
 
 int main() {
+<<<<<<< Upstream, based on origin/master
 	cout << "Here is conflict" << endl; // prints !!!Hello World!!!
 	cerr << "!!!Test My test Hello World!!!" << endl; // prints !!!Hello World!!!
+=======
+	std::cout << "!!!Test My test Hello World!!!" << endl; // prints !!!Hello World!!!
+>>>>>>> 8a450a1 using std::cout
 	// test 1
 	return 0;
 }
