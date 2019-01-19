ab4b47b22684d267fb63e153c38fd6912e870b0b
Mikhail Tchernychev <mikhail_tchernyhev@yahoo.com>
2019-01-18 19:51:53 -0800

    additional commit

--- src/test1.cpp
+++ src/test1.cpp
@@ -11,11 +11,16 @@ using namespace std;
 
 int main() {
 <<<<<<< Upstream, based on origin/master
+<<<<<<< Upstream, based on origin/master
 	cout << "Here is conflict" << endl; // prints !!!Hello World!!!
 	cerr << "!!!Test My test Hello World!!!" << endl; // prints !!!Hello World!!!
 =======
 	std::cout << "!!!Test My test Hello World!!!" << endl; // prints !!!Hello World!!!
 >>>>>>> 8a450a1 using std::cout
+=======
+	// my change
+	cerr << "!!!Tsasas est My test Hello World!!!" << endl; // prints !!!Hello World!!!
+>>>>>>> 752ed94 additional commit
 	// test 1
 	return 0;
 }
